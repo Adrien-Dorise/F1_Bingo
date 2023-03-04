4fd6a49647271f402ae39c04b4a7c077d00a858d
Adrien-Dorise <adrien.dorise@hotmail.com>
2023-03-04 23:03:28 +0100

    Added menu scene

---
+++ F1_BingoUnity/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public GameObject menuCanvas, optionCanvas, confirmationCanvas;
+    private string optionToReset;
+
+    private void Start()
+    {
+        menuCanvas.SetActive(true);
+        optionCanvas.SetActive(false);
+        confirmationCanvas.SetActive(false);
+        optionToReset = "";
+    }
+
+    public void raceButton()
+    {
+        SceneManager.LoadScene("Race");
+    }
+    
+    
+    public void seasonButton()
+    {
+        SceneManager.LoadScene("Season");
+        
+    }
+    
+    public void optionButton()
+    {
+        menuCanvas.SetActive(false);
+        optionCanvas.SetActive(true);
+    }
+    
+    public void resetRaceButton()
+    {
+        optionCanvas.SetActive(false);
+        confirmationCanvas.SetActive(true);
+        optionToReset = "race";
+    }
+    
+    public void resetSeasonButton()
+    {
+        optionCanvas.SetActive(false);
+        confirmationCanvas.SetActive(true);
+        optionToReset = "season";
+    }
+    
+    public void backButton()
+    {
+        optionCanvas.SetActive(false);
+        menuCanvas.SetActive(true);
+    }
+    
+    public void yesButton()
+    {
+        PlayerPrefs.DeleteKey(optionToReset);
+        confirmationCanvas.SetActive(false);
+        optionCanvas.SetActive(true);
+    }
+    
+    public void noButton()
+    {
+        confirmationCanvas.SetActive(false);
+        optionCanvas.SetActive(true);
+    }
+
+}
