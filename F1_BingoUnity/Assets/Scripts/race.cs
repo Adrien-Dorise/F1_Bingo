5e53784d2149a28d2ef7fa3dde21f0a383098ac4
Adrien-Dorise <adrien.dorise@hotmail.com>
2023-03-06 22:31:19 +0100

    Fusion of race and season scripts

--- F1_BingoUnity/Assets/Scripts/race.cs
+++ F1_BingoUnity/Assets/Scripts/race.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 using UnityEngine.Rendering.Universal;
 
 /// <summary>
-/// Class <c>selectionMenu</c> to attach to a Game Manager gameObject in the race scene.
+/// Class <c>selectionMenu</c> to attach to a Game Manager gameObject in the race/season scene.
 /// This class set all button behaviour for the race screen.
 /// </summary>
 public class race : MonoBehaviour
@@ -16,23 +16,45 @@ public class race : MonoBehaviour
     private List<GameObject> buttons; //Reference to all butotns in the scene
     private List<int> selectedButtons; //Store the state of eac buttons in the scene
     private GameObject RDVvirage; //Reference to RDVvirage bingo element
+    private bool isRace; //true if we are on race screen, false for season screen
+    private string saveStatus; //Store the playerkeys value according to current screen (race / season)
+    private int lightsPerBingo; //Numbe of lights lit per bingo selected
 
 
     private void Start()
     {
-        Debug.Log(PlayerPrefs.GetString(Save.race));
-        Debug.Log(PlayerPrefs.GetString(Save.racestatus));
+        isRace = SceneManager.GetActiveScene().name == "Race";
+        if(isRace)
+        {
+            lightsPerBingo = 6;
+            saveStatus = Save.racestatus;
+            Debug.Log(PlayerPrefs.GetString(Save.race));
+        }
+        else
+        {
+            lightsPerBingo = 1;
+            saveStatus = Save.season;
+            if(!PlayerPrefs.HasKey(Save.season)) //In case there is not season save stored
+            {
+                PlayerPrefs.SetString(Save.season, "".PadRight(GameObject.Find("Buttons").transform.childCount, '0')); //Add as many zeros as buttons on the scene.
+            }        
+        }
+        Debug.Log(PlayerPrefs.GetString(saveStatus));
 
         RDVvirage = GameObject.Find("RDVvirage");
         buttons = new List<GameObject>();
         selectedButtons = new List<int>();
         
-        string savedButtStates = PlayerPrefs.GetString(Save.season); 
+
+        string savedButtStates = PlayerPrefs.GetString(saveStatus); 
         int id = 1;
         foreach(Button butt in GameObject.Find("Buttons").transform.GetComponentsInChildren<Button>())
         {
             buttons.Add(butt.gameObject);
-            //butt.image.sprite = bingoImage[int.Parse(savedButt.Split(' ')[id])-1];
+            if(isRace)
+            {
+                butt.image.sprite = bingoImage[int.Parse(PlayerPrefs.GetString(Save.race).Split(' ')[id])-1];
+            }
             
             if(savedButtStates[id-1] == '0')
             {
@@ -59,19 +81,19 @@ public class race : MonoBehaviour
     /// </summary>
     public void bingoButton(int ID)
     {
-        System.Text.StringBuilder saveState = new System.Text.StringBuilder(PlayerPrefs.GetString(Save.racestatus));
+        System.Text.StringBuilder saveState = new System.Text.StringBuilder(PlayerPrefs.GetString(saveStatus));
         if(!selectedButtons.Contains(ID)) //The button was previously off and put on on click
         {
             selectedButtons.Add(ID);
             buttons[ID-1].GetComponent<Image>().color = Color.green;
             saveState[ID-1] = '1';
-            PlayerPrefs.SetString(Save.racestatus, saveState.ToString());
+            PlayerPrefs.SetString(saveStatus, saveState.ToString());
         }
         else
         {
             buttons[ID-1].GetComponent<Image>().color = Color.white;
             saveState[ID-1] = '0';
-            PlayerPrefs.SetString(Save.racestatus, saveState.ToString());
+            PlayerPrefs.SetString(saveStatus, saveState.ToString());
             try
             {   
                 selectedButtons.Remove(ID);
@@ -87,13 +109,13 @@ public class race : MonoBehaviour
     
     /// <summary>
     /// Method <c>RDVvirageManagement</c> manage the RDVvirage bingo card. This function is to call after each bingo card click.
-    /// More specifically, it verifies that the red/green lights are in check with the number of selected bingo
+    /// More specifically, it verifies that the red/green lights are in check with the number of selected bingos
     /// </summary>   
     private void RDVvirageManagement()
     {
         for(int i = 0 ; i < RDVvirage.transform.childCount; i++)
         {
-            if(i < selectedButtons.Count * 6)
+            if(i < selectedButtons.Count * lightsPerBingo)
             {
                 RDVvirage.transform.GetChild(i).GetComponent<Light2D>().color = Color.green;
             }
